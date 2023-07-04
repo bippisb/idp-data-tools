06e18c954809cdfa17c10a0fe3a6fb975147f934
Vijay Kumar <vijaynyaya@gmail.com>
2023-07-04 16:30:56 +0530

    add dataset id creator

--- IDP_Data_Tools.py
+++ IDP_Data_Tools.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(
     page_title="IDP Data Tools",
-    page_icon="👋",
+    page_icon="🛠",
 )
 
 st.write("# IDP Data Tools")
---
+++ pages/Dataset_ID_Creator.py
@@ -0,0 +1,61 @@
+import streamlit as st
+import string
+import random
+
+st.header("Dataset ID Creator")
+
+granularities = {
+    "India": "IN",
+    "State": "ST",
+    "District": "DT",
+    "Tehsil": "TH",
+    "Block": "BL",
+    "Sub-District": "SD",
+    "Gram Panchayat": "GP",
+    "City": "CT",
+    "Village": "VL",
+    "Local Body": "LB",
+    "Assembly Constituency": "AC",
+    "Parliamentary Constituency": "PC",
+    "Other Level": "OL",
+}
+granularity = st.selectbox(
+    "What's the granularity of the dataset (Spatial Resolution)?",
+    granularities.keys()
+)
+
+frequencies = {
+    "Daily": "D",
+    "Weekly": "W",
+    "Fortnightly": "F",
+    "Monthly": "M",
+    "Quarterly": "Q",
+    "Yearly": "Y",
+    "Quinquennial": "Q",
+    "Decadal": "D",
+    "Other / One Time": "O",
+}
+
+frequency = st.selectbox(
+    "What's the frequency of the dataset (Temporal Resolution)?",
+    frequencies.keys()
+)
+
+dataset_name = st.text_input(
+    "What's the name of the dataset? Don't use spaces or special characters.", max_chars=12)
+source_name = st.text_input(
+    "What's the name of the source of the dataset? Don't use spaces or special characters.", max_chars=6)
+
+
+def generate_dataset_id(name: str, source: str, granularity: str, frequency: str):
+    suffix = "".join(random.choices(string.ascii_lowercase, k=3))
+    id = "-".join((name, source, granularity, frequency, suffix))
+    return id.strip().replace(" ", "").lower()
+
+
+st.success(generate_dataset_id(
+    dataset_name,
+    source_name,
+    granularities[granularity],
+    frequencies[frequency]
+))
