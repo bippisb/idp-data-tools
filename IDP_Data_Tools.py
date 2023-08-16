0ba84783e2cb8b0b67884767b854902d7abd1903
Vijay Kumar <78458631+vijaynyaya@users.noreply.github.com>
2023-08-16 15:23:09 +0530

    add link to dataset QA

--- IDP_Data_Tools.py
+++ IDP_Data_Tools.py
@@ -12,6 +12,7 @@ st.markdown(
     This page hosts the streamlit based data tools used by the data team of India Data Portal.
     - Codebook Critic
     - Dataset ID Generator (SKU)
+    - Dataset QA
     
     Coming Soon
     - Codebook Generator
