93ecaab7bd3ecf9829ca93ecd9fbd707f9d2955d
kkhatke <113087478+kkhatke@users.noreply.github.com>
2023-08-17 09:58:45 +0530

    Update script
    Add Date data type in Change in datatype.
    Add Summary Statistics. 
    Add function Docstring.

--- pages/Dataset_QA.py
+++ pages/Dataset_QA.py
@@ -5,6 +5,14 @@ import urllib.parse
 import csv
 
 def get_special_char_count(column, special_chars_dict):
+    """
+    Calculates the count of special characters in a given column.
+    Parameters:
+        column (pandas.Series): The column to count the special characters from.
+        special_chars_dict (dict): A dictionary to store the special characters for each column.
+    Returns:
+        int: The count of special characters in the column.
+    """
     if column.dtype == "object":
         special_chars = [char for char in column.unique() if not str(char).isalnum()]
         special_chars_dict[column.name] = special_chars
@@ -12,6 +20,15 @@ def get_special_char_count(column, special_chars_dict):
     return 0
 
 def generate_dqa_info(data, col, special_chars):
+    """
+    Generate the data quality assessment (DQA) information for a given column.
+    Parameters:
+        data (pandas.DataFrame): The dataframe containing the data.
+        col (str): The name of the column for which to generate the DQA information.
+        special_chars (list): A list of special characters.
+    Returns:
+        str: The DQA information for the column.
+    """
     dqa_info = f"### Column: {col}\n"
     dqa_info += f"Data Type: {data[col].dtype}\n"
     dqa_info += f"Number of Numerical Values: {data[col].apply(pd.to_numeric, errors='coerce').notnull().sum()}\n"
@@ -19,9 +36,24 @@ def generate_dqa_info(data, col, special_chars):
     dqa_info += f"Count of Unique Values: {data[col].nunique()}\n"
     dqa_info += f"Count of Special Characters: {len(special_chars) if special_chars is not None and isinstance(special_chars, list) else 0}\n"
     dqa_info += f"Unique Values: {', '.join(map(str, data[col].unique()))}\n\n"
+
+     # Include formatting details if applicable
+    if col == "state_code" or col == "district_code" or col == "block_code" or col == "gp_code"  or col == "village_code" or col == "sub_district_code" or col == "subdistrict_code":
+        dqa_info += f"Formatting: {col} values were formatted to have leading zeros.\n"
+    
+    dqa_info += "\n"
     return dqa_info
 
 def generate_dqa_special_chars_info(col, special_chars):
+    """
+    Generate the information about special characters in a given column.
+    Parameters:
+        col (str): The name of the column.
+        special_chars (list): A list of special characters.
+    Returns:
+        str: A formatted string containing the column name and the special character values.
+            Returns an empty string if no special characters are provided.
+    """
     if special_chars:
         dqa_info = f"### Column: {col}\n"
         dqa_info += f"Special Character Values: {', '.join(map(str, special_chars))}\n\n"
@@ -29,15 +61,51 @@ def generate_dqa_special_chars_info(col, special_chars):
     return ""
 
 def generate_dqa_duplicate_info(duplicate_count):
+    """
+    Generates a description of the number of duplicate rows in the dataset.
+    Parameters:
+        duplicate_count (int): The number of duplicate rows in the dataset.
+    Returns:
+        str: A string containing the number of duplicate rows.
+    """
     return f"## Number of Duplicate Rows\nNumber of Duplicate Rows: {duplicate_count}\n\n"
 
 def generate_dqa_changes_summary(changes):
-    dqa_info = "## Data Type Changes Summary\n"
+    """
+    Generate a summary of the data type changes and other changes.
+    Parameters:
+    - changes (dict): A dictionary containing the column names as keys and the changes as values.
+    Returns:
+    - dqa_info (str): A string containing the summary of data type changes and other changes.
+    """
+    dqa_info = "## Data Type Changes and Other Changes Summary\n"
     for col, change in changes.items():
-        dqa_info += f"Column: {col}\nChange: {change}\n\n"
+        dqa_info += f"Column/Change: {col}\nChange: {change}\n\n"
+    return dqa_info
+
+def generate_dqa_summary_statistics(summary_statistics):
+    """
+    Generates the summary statistics for the given summary_statistics.
+    Parameters:
+    - summary_statistics (str): A string containing the summary statistics.
+    Returns:
+    - dqa_info (str): A string containing the summary statistics for numerical columns.
+    """
+    dqa_info = "## Summary Statistics for Numerical Columns\n"
+    # Use to_string() to format the table
+    dqa_info += summary_statistics.to_string()  
+    dqa_info += "\n\n"
     return dqa_info
 
 def get_download_link(file_name, content_type):
+    """
+    Generates a download link for a given file.
+    Parameters:
+        file_name (str): The name of the file to be downloaded.
+        content_type (str): The MIME type of the file.
+    Returns:
+        str: The HTML code for the download link.
+    """
     with open(file_name, "rb") as f:
         data = f.read()
     base64_data = base64.b64encode(data).decode("utf-8")
@@ -46,10 +114,16 @@ def get_download_link(file_name, content_type):
     return href
 
 def main():
+    """
+    The main function of the app that handles the dataset QA process.
+    Parameters:
+    None
+    Returns:
+    None
+    """
     st.set_page_config(page_title="Dataset QA")
     st.title("Dataset QA App")
-  
-    
+   
     # File upload
     uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
     
@@ -79,15 +153,29 @@ def main():
             st.write(f"Count of Special Characters: {special_chars}")
             st.write(f"Unique Values: {data[col].unique()}")
             
-            # Change datatype
-            new_dtype = st.selectbox(f"Change Data Type for {col}:", ["No Change", "int", "float", "str"], key=f"{col}_dtype")
-            if new_dtype != "No Change":
-                try:
-                    data[col] = data[col].astype(new_dtype)
-                    changes[col] = f"Data Type Changed to {new_dtype}"
-                except:
-                    changes[col] = "Error: Unable to change data type"
+            # Format columns
+            format_rules = {
+                "state_code": 2, "district_code": 3, "sub_district_code": 4,
+                "block_code": 4, "village_code": 6, "gp_code": 6
+            }
+            if col in format_rules:
+                data[col] = data[col].apply(lambda x: str(x).zfill(format_rules[col]))
+                st.write(f"#### {col} values were formatted to have leading zeros.")
             
+
+            # Change datatype
+            new_dtype = st.selectbox(f"Change Data Type for {col}:", ["No Change", "int", "float", "str", "date"], key=f"{col}_dtype")
+            if new_dtype != "No Change":
+                if new_dtype == "date":
+                    data[col] = pd.to_datetime(data[col], errors='coerce', format='%d-%m-%Y')
+                    changes[col] = f"Data Type Changed to {new_dtype} (Format: dd-mm-yyyy)"
+                else:
+                    try:
+                        data[col] = data[col].astype(new_dtype)
+                        changes[col] = f"Data Type Changed to {new_dtype}"
+                    except:
+                        changes[col] = "Error: Unable to change data type"
+                        
             dqa_report += generate_dqa_info(data, col, special_chars)
             st.write("---")
         
@@ -105,30 +193,24 @@ def main():
         duplicate_count = data.duplicated().sum()
         st.write(f"Number of Duplicate Rows: {duplicate_count}")
         dqa_report += generate_dqa_duplicate_info(duplicate_count)
-        
+
         # Remove duplicate rows
         if st.button("Remove Duplicate Rows"):
             data.drop_duplicates(inplace=True)
             st.success("Duplicate rows removed.")
-            
-        # Format state_code and district_code columns
-        if "state_code" in data.columns:
-            data["state_code"] = data["state_code"].apply(lambda x: str(x).zfill(2))
-        if "district_code" in data.columns:
-            data["district_code"] = data["district_code"].apply(lambda x: str(x).zfill(3))
-        if "subdistrict_code" in data.columns:
-            data["subdistrict_code"] = data["subdistrict_code"].apply(lambda x: str(x).zfill(4))
-        if "sub_district_code" in data.columns:
-            data["sub_district_code"] = data["sub_district_code"].apply(lambda x: str(x).zfill(4))
-        if "block_code" in data.columns:
-            data["block_code"] = data["block_code"].apply(lambda x: str(x).zfill(4))
-        if "village_code" in data.columns:
-            data["village_code"] = data["village_code"].apply(lambda x: str(x).zfill(6))
-        if "gp_code" in data.columns:
-            data["gp_code"] = data["gp_code"].apply(lambda x: str(x).zfill(6))
-        
+            changes["Duplicate Rows"] = "Duplicate rows were removed."
+
+        # Summary statistics
+        st.write("## Summary Statistics for Numerical Columns")
+        numerical_columns = data.select_dtypes(include=["int64", "float64"]).columns
+        summary_statistics = data[numerical_columns].describe()
+        st.write(summary_statistics)
+
+        dqa_report += generate_dqa_summary_statistics(summary_statistics)
+
+        # Changes summary
         dqa_report += generate_dqa_changes_summary(changes)
-            
+
         # Download updated dataset
         st.write("## Download Updated Dataset")
         if st.button("Download"):
